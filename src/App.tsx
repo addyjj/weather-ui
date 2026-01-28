24bf7005798c6e27918478a00e31984e0c307be1
Jeremy Addy <addyjj@outlook.com>
2026-01-27 19:44:30 -0600

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router";
 import { Home } from "./pages/home/Home";
+import "./App.css";
 
 function App() {
   return (
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,3 @@
 export function Home() {
-  return <div>Home Page</div>;
+  return <h1>Home Page</h1>;
 }
